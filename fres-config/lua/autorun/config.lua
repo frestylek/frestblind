2265a19c8d45d63c806252fb024df8e9821bb991
frestylek <42667570+frestylek@users.noreply.github.com>
2022-08-03 12:10:15 +0200

    Update config.lua

--- fres-config/lua/autorun/config.lua
+++ fres-config/lua/autorun/config.lua
@@ -4,7 +4,7 @@ fresConfig.jobs = fresConfig or {}
 hook.Add( "Initialize", "fresConfig", function()
 
 	fresConfig.jobs = {
-		[TEAM_HOBO] = true,
+		[TEAM_HOBO] = true, -- Here is template for blinded jobs
 	}
 
 end )
